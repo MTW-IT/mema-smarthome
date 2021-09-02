e641a8370360d44863ca77f2b09d11310e61e298
TimTomczak <41620700+TimTomczak@users.noreply.github.com>
2021-09-02 15:18:31 +0200

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,152 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+	  $('#url').hide(); 
+    });
+
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+
+    // HOME SLIDER & COURSES & CLIENTS
+    $('.home-slider').owlCarousel({
+      animateOut: 'fadeOut',
+      items:1,
+      loop:true,
+      dots:false,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+    })
+
+    $('.owl-courses').owlCarousel({
+      animateOut: 'fadeOut',
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+      dots: false,
+      nav:true,
+      navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+      ],
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        1000: {
+          items: 3,
+        }
+      }
+    });
+
+    $('.owl-client').owlCarousel({
+      animateOut: 'fadeOut',
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        1000: {
+          items: 3,
+        }
+      }
+    });
+
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.custom-navbar a, #home a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    }); 
+
+	 // Get the form.
+    var form = $('#contact-form');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+
+		// Set up an event listener for the contact form.
+	$("#contact-form").submit(function(event) {
+		// Stop the browser from submitting the form.
+		event.preventDefault();
+		
+		if($('#url').val() != ''){
+			console.log('URL Field set');
+			return;			
+		}
+		
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			console.log("done");
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('alert alert-danger');
+			$(formMessages).addClass('alert alert-success');
+			$('#submit').prop('disabled', true);
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#name').val('');
+			$('#email').val('');
+			$('#message').val('');
+		})
+		.fail(function(data) {
+			console.log("fail");
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('alert alert-success');
+			$(formMessages).addClass('alert alert-danger');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+
+	});
+	
+	
+	
+	
+	
+	
+
+})(jQuery);
